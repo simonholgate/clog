a3bea3d5828ad2ff6228339f67bd11d5d4854cad
Simon Holgate <git@simonholgate.org.uk>
2013-01-28 12:31:45 +0000

    added testing code to try and resolve db connection error

--- diy/clog/src/clog/models.clj
+++ diy/clog/src/clog/models.clj
@@ -1,22 +1,22 @@
 (ns clog.models
   (:use korma.db
         korma.core)
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as string])
   (:import (java.net URI)))
 
 ;; (comment from Google groups post: https://groups.google.com/forum/?fromgroups=#!searchin/sqlkorma/heroku/sqlkorma/KdwAU0KfiU0/VhyTlnkA5nYJ
-;; (def db-uri (java.net.URI. (System/getenv "DATABASE_URL"))) 
+(def db-uri (java.net.URI. (System/getenv "DATABASE_URL"))) 
 
-;; (def user-and-password (string/split (.getUserInfo db-uri) #":")) 
+(def user-and-password (string/split (.getUserInfo db-uri) #":")) 
 
-;; (def clogdb {:classname "org.postgresql.Driver" 
-;;          :subprotocol "postgresql" 
-;;          :user (get user-and-password 0) 
-;;          :password (get user-and-password 1) ; may be nil 
-;;          :subname (if (= -1 (.getPort db-uri)) 
-;;                     (format "//%s%s" (.getHost db-uri) (.getPath db-uri)) 
-;;                     (format "//%s:%s%s" (.getHost db-uri) (.getPort 
-;; db-uri) (.getPath db-uri)))})
+(def clogdb {:classname "org.postgresql.Driver" 
+         :subprotocol "postgresql" 
+         :user (get user-and-password 0) 
+         :password (get user-and-password 1) ; may be nil 
+         :subname (if (= -1 (.getPort db-uri)) 
+                    (format "//%s%s" (.getHost db-uri) (.getPath db-uri)) 
+                    (format "//%s:%s%s" (.getHost db-uri) (.getPort 
+db-uri) (.getPath db-uri)))})
 
 
 ;; (comment connection info from http://www.leonardoborges.com/writings/2012/09/10/clojure-leiningen-heroku-aot-compilation-gotchas/ and env variables from http://www.jisaacks.com/setup-cakephp-app-on-heroku)
@@ -26,25 +26,31 @@
 ;;       :user (System/getenv "DB_USER")
 ;;       :password (System/getenv "DB_PASSWORD")
 ;;       :subname (System/getenv "DATABASE_URL")})
-(defn cloud-db
-  "Generate the db map according to cloud environment when available."
-  []
-  (when (System/getenv "DATABASE_URL")
-    (let [url (URI. (System/getenv "DATABASE_URL"))
-          host (.getHost url)
-          port (if (pos? (.getPort url)) (.getPort url) 5432)
-          path (.getPath url)]
-      (merge
-       {:subname (str "//" host ":" port path)}
-       (when-let [user-info (.getUserInfo url)]
-         {:user (first (str/split user-info #":"))
-          :password (second (str/split user-info #":"))})))))
 
-(def clogdb
-  (merge {:classname "org.postgresql.Driver"
-          :subprotocol "postgresql"
-          :subname "//localhost:5432/px"}
-         (cloud-db)))
+;; (defn cloud-db
+;;   "Generate the db map according to cloud environment when available."
+;;   []
+;;   (when (System/getenv "DATABASE_URL")
+;;     (let [url (URI. (System/getenv "DATABASE_URL"))
+;;           host (.getHost url)
+;;           port (if (pos? (.getPort url)) (.getPort url) 5432)
+;;           path (.getPath url)]
+;;       (merge
+;;        {:subname (str "//" host ":" port path)}
+;;        (when-let [user-info (.getUserInfo url)]
+;;          {:user (first (str/split user-info #":"))
+;;           :password (second (str/split user-info #":"))})))))
 
-(defentity authors)
-(defentity posts)
+;; (def clogdb
+;;   (merge {:classname "org.postgresql.Driver"
+;;           :subprotocol "postgresql"
+;;           :subname "//localhost:5432/px"}
+;;          (cloud-db)))
+
+(defn -main []
+  (print "Connecting to database...") (flush)
+  (defentity authors)
+  (defentity posts)
+  (insert  authors 
+           (values {:id 1, :username "vijay", :password "password", :email "mail AT vijaykiran.com"}) )
+  (println " done"))
