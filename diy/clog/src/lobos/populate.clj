fc72f1150e7b2461d2a7e6ebbcfb6f79610ce67e
Simon Holgate <git@simonholgate.org.uk>
2013-01-28 07:07:15 +0000

    added lobos.populate to add data to database

---
+++ diy/clog/src/lobos/populate.clj
@@ -0,0 +1,12 @@
+(comment "This file populates the database. It can be run from the shell
+  with lein run -m 'lobos.populate
+          ")
+
+(ns lobos.populate
+  (:use 'clj-yaml.core))
+
+
+(defn -main []
+  (print "Populating database...") (flush)
+  (parse-string (slurp "./resources/fixtures.yml"))
+  (println " done"))
